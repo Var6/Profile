00d431bf3c77d41891f44a43e2db5190abd06b57
Rishabh Ranjan <rk.remo12345@gmail.com>
2024-04-11 16:50:57 +0530

    day1

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import link from 'next/link'
+function Navbar() {
+  return (
+    <nav>
+    <div className="">
+    <link>
+
+    </link>
+
+    </div>
+      
+    </nav>
+  )
+}
+
+export default Navbar
