06575120574b281d8badac5d02a058f34a6c66f9
Rishabh Ranjan <rk.remo12345@gmail.com>
2024-04-14 21:06:22 +0530

    day_three

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React, {useTransition, useState} from 'react'
 import Image from 'next/image'
+import TabButton from './TabButton';
 const AboutSection = () => {
     const [tab,settab]=useState("Skills");
     const [startTransition,ispending]=useTransition();
@@ -13,11 +14,12 @@ const AboutSection = () => {
   return (
     <section className='text-white'>
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16">
-        <Image src="www.googl" width={500} height={500} />
+        <Image src="" alt='image dumb ass' width={500} height={500} />
         <div className="">
             <h2 className='text-4xl font-bold text-white mb-4'>About Me</h2>
             <p className='text-base md:text-lg'> to be written about me </p>
             <div className="flex flex-row mt-8">
+              <TabButton selectTab={()=>handletabchange("Skills")} active={tab==="Skills"}>Skills</TabButton>
                 <span className='mr-3 font-semibold hover:text-white text-[#ADB7BE] border-b border-purple-500' > Skill     </span>
                 <span className='mr-3 font-semibold hover:text-white text-[#ADB7BE] border-b border-purple-500' > Education     </span>
                 <span className='mr-3 font-semibold hover:text-white text-[#ADB7BE] border-b border-purple-500' > Experience     </span>
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
 const TabButton = ({active,selectTab,Children}) => {
-    const buttonClasses = active ? 'text-white border-b border-white border-purple-500 ':'text-[#'
+    const buttonClasses = active ? 'text-white border-b border-white border-purple-500 ':'text-[#ADB7BE]'
   return (
-    <button className='mr-3 font-semibold hover:text-white text-[#ADB7BE] border-b border-purple-500' >
+    <button onClick={selectTab}>
+      <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}>
         {Children}
+      </p>
     </button>
   )
 }
